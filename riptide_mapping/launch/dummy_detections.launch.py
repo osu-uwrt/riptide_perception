7228c5160686c5996877a4618f717542dcd20ea9
Brach Knutson <brach2003@gmail.com>
2023-04-06 20:17:28 -0400

    scope dummy detections launch file

--- riptide_mapping/launch/dummy_detections.launch.py
+++ riptide_mapping/launch/dummy_detections.launch.py
@@ -1,7 +1,7 @@
 import os
 import launch
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch_ros.actions import Node, PushRosNamespace
 from launch.substitutions import LaunchConfiguration as LC
 
@@ -26,20 +26,22 @@ def generate_launch_description():
             description="name of the robot"
         ),
         
-        PushRosNamespace(
-            LC("robot")
-        ),
-
-        # create the nodes    
-        Node(
-            package='riptide_mapping2',
-            executable='dummydetections.py',
-            name='dummydetections',
-            respawn=True,
-            output='screen',
+        GroupAction(actions=[
+            PushRosNamespace(
+                LC("robot")
+            ),
             
-            parameters = [
-                config
-            ]
-        ),
+            # create the nodes    
+            Node(
+                package='riptide_mapping2',
+                executable='dummydetections.py',
+                name='dummydetections',
+                respawn=True,
+                output='screen',
+                
+                parameters = [
+                    config
+                ]
+            )
+        ], scoped=True)
     ])
