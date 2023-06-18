f6a449f7f059bf2f7510b01b01e0f15bd02be9c6
tobymcguire0 <tobymcguire461@gmail.com>
2023-06-18 10:11:21 -0400

    Added logic to get 2D bounding box

--- training/data_generator.py
+++ training/data_generator.py
@@ -52,7 +52,7 @@ def main():
     parser.add_argument(
         "-t", "--train", dest="training_obj", metavar='FILE',
         help=(
-        "Name of the object you wan't to generate data for."
+        "Name of the object you want to generate data for."
         "\n Supported names: " + training_names_helper
         ),
     )
@@ -62,7 +62,7 @@ def main():
     )
     parser.add_argument(
         "-n", "--number", dest="number_datapoints", type=int,
-        help="Location to store generated files", default=1,
+        help="Number of datapoints to generate", default=1,
     )
 
     args = parser.parse_args(argv)  # In this example we won't use the args
@@ -94,34 +94,132 @@ def main():
         # Render image
         bpy.context.scene.render.filepath = str(img_output.resolve())
         bpy.ops.render.render(write_still=True)
-
-        # need to look at this to compute proper bbox
-        # https://blender.stackexchange.com/questions/185296/find-the-size-of-an-object-in-camera-frame-for-bounding-box-creation
-        # the bbox method below spits out world cordinates for the 8 verticies of the 3d bbox
-        # this is another similar version 
-        # https://blender.stackexchange.com/questions/7198/save-the-2d-bounding-box-of-an-object-in-rendered-image-to-a-text-file/
         
         # Get object's 2D image bounding box
         obj = bpy.data.objects[args.training_obj]
         print(obj.bound_box)
-        bbox_2d = obj.bound_box[:]
-        bbox_2d = [(bbox_2d[i][0], bbox_2d[i][1]) for i in range(8)]
-        print(bbox_2d)
-        min_x = min([p[0] for p in bbox_2d])
-        max_x = max([p[0] for p in bbox_2d])
-        min_y = min([p[1] for p in bbox_2d])
-        max_y = max([p[1] for p in bbox_2d])
+
+        #https://blender.stackexchange.com/questions/7198/save-the-2d-bounding-box-of-an-object-in-rendered-image-to-a-text-file/
+        bbox_2d = camera_view_bounds_2d(bpy.context.scene, bpy.context.scene.camera, obj)
         
         # Output 2D bounding box to text file
         with open(label_output, "w") as f:
-            x_center = (min_x + max_x) / 2
-            y_center = (min_y + max_y) / 2
-            width = max_x - min_x
-            height = max_y - min_y
-            f.write(f"0 {x_center} {y_center} {width} {height}\n")       
+            f.write(f"0 {bbox_2d.x} {bbox_2d.y} {bbox_2d.width} {bbox_2d.height}\n")       
         
 
     print("Generation finished, exiting")
+
+
+class Box:
+
+    dim_x = 1
+    dim_y = 1
+
+    def __init__(self, min_x, min_y, max_x, max_y, dim_x=dim_x, dim_y=dim_y):
+        self.min_x = min_x
+        self.min_y = min_y
+        self.max_x = max_x
+        self.max_y = max_y
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+
+    @property
+    def x(self):
+        return round(self.min_x * self.dim_x)
+
+    @property
+    def y(self):
+        return round(self.dim_y - self.max_y * self.dim_y)
+
+    @property
+    def width(self):
+        return round((self.max_x - self.min_x) * self.dim_x)
+
+    @property
+    def height(self):
+        return round((self.max_y - self.min_y) * self.dim_y)
+
+    def __str__(self):
+        return "<Box, x=%i, y=%i, width=%i, height=%i>" % \
+               (self.x, self.y, self.width, self.height)
+
+    def to_tuple(self):
+        if self.width == 0 or self.height == 0:
+            return (0, 0, 0, 0)
+        return (self.x, self.y, self.width, self.height)
+
+
+def camera_view_bounds_2d(scene, cam_ob, me_ob):
+    """
+    Returns camera space bounding box of mesh object.
+
+    Negative 'z' value means the point is behind the camera.
+
+    Takes shift-x/y, lens angle and sensor size into account
+    as well as perspective/ortho projections.
+
+    :arg scene: Scene to use for frame size.
+    :type scene: :class:`bpy.types.Scene`
+    :arg obj: Camera object.
+    :type obj: :class:`bpy.types.Object`
+    :arg me: Untransformed Mesh.
+    :type me: :class:`bpy.types.Mesh´
+    :return: a Box object (call its to_tuple() method to get x, y, width and height)
+    :rtype: :class:`Box`
+    """
+
+    mat = cam_ob.matrix_world.normalized().inverted()
+    me = me_ob.to_mesh(scene, True, 'PREVIEW')
+    me.transform(me_ob.matrix_world)
+    me.transform(mat)
+
+    camera = cam_ob.data
+    frame = [-v for v in camera.view_frame(scene=scene)[:3]]
+    camera_persp = camera.type != 'ORTHO'
+
+    lx = []
+    ly = []
+
+    for v in me.vertices:
+        co_local = v.co
+        z = -co_local.z
+
+        if camera_persp:
+            if z == 0.0:
+                lx.append(0.5)
+                ly.append(0.5)
+            # Does it make any sense to drop these?
+            #if z <= 0.0:
+            #    continue
+            else:
+                frame = [(v / (v.z / z)) for v in frame]
+
+        min_x, max_x = frame[1].x, frame[2].x
+        min_y, max_y = frame[0].y, frame[1].y
+
+        x = (co_local.x - min_x) / (max_x - min_x)
+        y = (co_local.y - min_y) / (max_y - min_y)
+
+        lx.append(x)
+        ly.append(y)
+
+    min_x = clamp(min(lx), 0.0, 1.0)
+    max_x = clamp(max(lx), 0.0, 1.0)
+    min_y = clamp(min(ly), 0.0, 1.0)
+    max_y = clamp(max(ly), 0.0, 1.0)
+
+    bpy.data.meshes.remove(me)
+
+    r = scene.render
+    fac = r.resolution_percentage * 0.01
+    dim_x = r.resolution_x * fac
+    dim_y = r.resolution_y * fac
+
+    return Box(min_x, min_y, max_x, max_y, dim_x, dim_y)
+
+
+def clamp(x, minimum, maximum):
+    return max(minimum, min(x, maximum))
 
 
 if __name__ == "__main__":
