d23a14d21ff38f571be4ab9563aeb36f739033f9
coalman321 <cjtucker321@gmail.com>
2023-06-26 23:21:58 -0400

    well somehow this works........ I am veery confused

--- tensor_detector/src/detector.cpp
+++ tensor_detector/src/detector.cpp
@@ -14,10 +14,7 @@
 #include <memory>
 #include <numeric>
 
-using namespace nvinfer1;
-using namespace nvonnxparser;
-
-class Logger : public ILogger
+class Logger : public nvinfer1::ILogger
 {
     void log(Severity sev, const char *msg) noexcept override
     {
@@ -38,130 +35,61 @@ size_t getSizeByDim(const nvinfer1::Dims &dims)
     return size;
 }
 
-void preprocessImage(const std::string &image_path, float *gpu_input, const nvinfer1::Dims &dims)
+struct InferDeleter
 {
-    cv::Mat frame = cv::imread(image_path);
-    if (frame.empty())
+    template <typename T>
+    void operator()(T *obj) const
     {
-        std::cerr << "Input image " << image_path << " load failed\n";
-        return;
-    }
-
-    // upload image to GPU
-    cv::cuda::GpuMat gpu_frame;
-    gpu_frame.upload(frame);
-
-    // resize the image to match the nn input tensor size
-    auto input_width = dims.d[2];
-    auto input_height = dims.d[1];
-    auto channels = dims.d[0];
-    auto input_size = cv::Size(input_width, input_height);
-    cv::cuda::GpuMat resized;
-    cv::cuda::resize(gpu_frame, resized, input_size, 0, 0, cv::INTER_NEAREST);
-
-    // pre-normalize
-    cv::cuda::GpuMat flt_image;
-    resized.convertTo(flt_image, CV_32FC3, 1.f / 255.f);
-    cv::cuda::subtract(flt_image, cv::Scalar(0.485f, 0.456f, 0.406f), flt_image, cv::noArray(), -1);
-    cv::cuda::divide(flt_image, cv::Scalar(0.229f, 0.224f, 0.225f), flt_image, 1, -1);
-
-    // send it into a tensor now
-    std::vector<cv::cuda::GpuMat> chw;
-    for (int i = 0; i < channels; ++i)
-    {
-        chw.emplace_back(cv::cuda::GpuMat(input_size, CV_32FC1, gpu_input + i * input_width * input_height));
-    }
-    cv::cuda::split(flt_image, chw);
-}
-
-void postprocessResults(float *gpu_output, const nvinfer1::Dims &dims)
-{
-    // get class names
-    std::vector<std::string> classes = {"class_a"};
-
-    // copy results from GPU to CPU
-    std::vector<float> cpu_output(getSizeByDim(dims));
-    cudaMemcpy(cpu_output.data(), gpu_output, cpu_output.size() * sizeof(float), cudaMemcpyDeviceToHost);
-
-    // calculate softmax
-    std::transform(cpu_output.begin(), cpu_output.end(), cpu_output.begin(), [](float val)
-                   { return std::exp(val); });
-    auto sum = std::accumulate(cpu_output.begin(), cpu_output.end(), 0.0);
-    // find top classes predicted by the model
-    std::vector<int> indices(getSizeByDim(dims));
-    // generate sequence 0, 1, 2, 3, ..., 999
-    std::iota(indices.begin(), indices.end(), 0);
-    std::sort(indices.begin(), indices.end(), [&cpu_output](int i1, int i2)
-              { return cpu_output[i1] > cpu_output[i2]; });
-    // print results
-    int i = 0;
-    while (cpu_output[indices[i]] / sum > 0.005)
-    {
-        if (classes.size() > indices[i])
+        if (obj)
         {
-            std::cout << "class: " << classes[indices[i]] << " | ";
+            obj->destroy();
         }
-        std::cout << "confidence: " << 100 * cpu_output[indices[i]] / sum << "% | index: " << indices[i] << "n";
-        ++i;
     }
-}
+};
 
-// size_t loadEngine(std::string engine_file, void* serialized_engine){
-//     std::ifstream file(engine_file.c_str(), std::ios::binary);
-//     if (!file.good())
-//     {
-//         throw std::runtime_error("Failed to open file: " + engine_file);
-//     }
+template <typename T>
+using UniquePtr = std::unique_ptr<T, InferDeleter>;
 
-//     // get the size of the file and seek back
-//     size_t size = 0;
-//     file.seekg(0, file.end);
-//     size = file.tellg();
-//     file.seekg(0, file.beg);
+class YoloInfer
+{
+private:
+    // TRT runtime system data
+    UniquePtr<nvinfer1::ICudaEngine> enginePtr;
+    UniquePtr<nvinfer1::IExecutionContext> contextPtr;
 
-//     serialized_engine = new char[size];
-//     file.read(serialized_engine, size);
-//     file.close();
+public:
+    YoloInfer(const std::string &engine_file_path)
+    {
+        std::ifstream engineFile(engine_file_path, std::ios::binary);
 
-//     return size;
-// }
+        // make sure we can open it
+        if (engineFile.fail())
+        {
+            return;
+        }
 
-size_t createEngine(std::string onnx_file, void* serialized_engine){
-    //define trt flags for network
-    uint32_t flags = 1U << static_cast<uint32_t>(NetworkDefinitionCreationFlag::kEXPLICIT_BATCH);
+        // Read the binary file
+        engineFile.seekg(0, std::ifstream::end);
+        auto engineSize = engineFile.tellg();
+        engineFile.seekg(0, std::ifstream::beg);
 
-    IBuilder * builder = createInferBuilder(logger);
-    INetworkDefinition * network = builder->createNetworkV2(flags); 
-    IParser * parser = createParser(*network, logger);
+        std::vector<char> engineData(engineSize);
+        engineFile.read(engineData.data(), engineSize);
 
-    // load a model
-    parser->parseFromFile(onnx_file.c_str(), static_cast<uint32_t>(ILogger::Severity::kINFO));
-    
-    // check for errors
-    for(size_t i = 0; i < parser->getNbErrors(); i++){
-        std::cout << parser->getError(i)->desc() << std::endl;
+        // Try create the TRT Runtime and load the engine
+        UniquePtr<nvinfer1::IRuntime> runtime{nvinfer1::createInferRuntime(logger)};
+        enginePtr.reset(runtime->deserializeCudaEngine(engineData.data(), engineSize, nullptr));
+        assert(enginePtr.get() != nullptr);        
+
+        // load the execution context
+        contextPtr.reset(enginePtr->createExecutionContext());
+        assert(contextPtr.get() != nullptr);
     }
 
-    printf("onnx file loaded\n");
-
-    // configure the model builder
-    IBuilderConfig * builderConfig = builder->createBuilderConfig();
-
-    // serialize the network
-    IHostMemory * serializedModel = builder->buildSerializedNetwork(*network, *builderConfig);
-
-    printf("Engine created\n");
-
-    // memory leak w/o this :/
-    delete builderConfig;
-    delete parser;
-    delete network;
-    delete builder;
-
-    serialized_engine = serializedModel->data();
-
-    return serializedModel->size();  
-}
+    ~YoloInfer()
+    {
+    }
+};
 
 int main(int argc, char **argv)
 {
@@ -172,67 +100,10 @@ int main(int argc, char **argv)
 
     // model info
     std::string input_file = "durr.png";
-    // std::string engine_file = "yolo.engine";
-    std::vector<std::string> input_blobs = {};
-    std::vector<std::string> output_blobs = {};
+    std::string engine_file = "yolo.engine";
 
-    // open the file
-    // char *engine_data;
-    // size_t engine_size = loadEngine(engine_file, engine_data);
-
-    std::string modelFile = "best.onnx";
-
-    // open the file
-    void *engine_data;
-    size_t engine_size = createEngine(modelFile, engine_data);
-
-
-    // make the inference runtime
-    std::unique_ptr<IRuntime> runtime = std::unique_ptr<IRuntime>(createInferRuntime(logger));
-
-    // load the model and context
-    printf("Deserializing engine\n");
-    std::unique_ptr<ICudaEngine> engine = std::unique_ptr<ICudaEngine>(runtime->deserializeCudaEngine(engine_data, engine_size));
-    std::unique_ptr<IExecutionContext> context = std::unique_ptr<IExecutionContext>(engine->createExecutionContext());
-
-    // configure the context
-    context->setDebugSync(true);
-
-    // try to get the io dims
-    printf("Binding I/O\n");
-    std::vector<nvinfer1::Dims> input_dims;               // we expect only one input
-    std::vector<nvinfer1::Dims> output_dims;              // and one output
-    std::vector<void *> buffers(engine->getNbBindings()); // buffers for input and output data
-    for (int i = 0; i < engine->getNbBindings(); ++i)
-    {
-        auto binding_size = getSizeByDim(engine->getBindingDimensions(i)) * sizeof(float);
-        cudaMalloc(&buffers[i], binding_size);
-        if (engine->bindingIsInput(i))
-        {
-            input_dims.emplace_back(engine->getBindingDimensions(i));
-        }
-        else
-        {
-            output_dims.emplace_back(engine->getBindingDimensions(i));
-        }
-    }
-    if (input_dims.empty() || output_dims.empty())
-    {
-        std::cerr << "Expect at least one input and one output for networkn";
-        return -1;
-    }
-
-    // preprocess input data
-    preprocessImage(input_file, (float *)buffers[0], input_dims[0]);
-    // inference
-    context->enqueue(1, buffers.data(), 0, nullptr);
-    // post-process results
-    postprocessResults((float *)buffers[1], output_dims[0]);
-
-    for (void *buf : buffers)
-    {
-        cudaFree(buf);
-    }
+    // Load the inference engine and context
+    YoloInfer infer = YoloInfer(engine_file);
 
     return 0;
 }
