b655bb80916752b3664dd9307996182d854b5d09
coalman321 <cjtucker321@gmail.com>
2023-07-27 00:37:05 -0400

    beat this borrowed library into shape

--- tensor_detector/src/tensorrtWrapper.cpp
+++ tensor_detector/src/tensorrtWrapper.cpp
@@ -2,16 +2,16 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/qos.hpp"
-// #include "cv_bridge/cv_bridge.h"
+#include "cv_bridge/cv_bridge.h"
 #include "sensor_msgs/msg/image.hpp"
 #include "geometry_msgs/geometry_msgs/msg/pose.hpp"
 #include "vision_msgs/vision_msgs/msg/object_hypothesis_with_pose.hpp"
 #include "vision_msgs/vision_msgs/msg/detection3_d.hpp"
 #include "vision_msgs/vision_msgs/msg/detection3_d_array.hpp"
 
-using namespace std::chrono_literals;
+#include "tensorrt_detector/yolov5_detector.hpp"
 
-#include "tensor_detector/detector.hpp"
+using namespace std::chrono_literals;
 
 class TensorrtWrapper : public rclcpp::Node
 {
@@ -19,103 +19,119 @@ public:
     TensorrtWrapper()
         : Node("tensorrt_wrapper")
     {
-        rclcpp::QoS video_qos(10);
-
         std::string engine_file = "yolo.engine";
 
-        infer = std::make_shared<tensor_detector::YoloInfer>(engine_file);
-        detection_pub = this->create_publisher<vision_msgs::msg::Detection3DArray>("tensorrt/detections", 1);
-        left_image_sub = this->create_subscription<sensor_msgs::msg::Image>("/zed2i/zed_node/left/image_rect_color", video_qos, std::bind(&TensorrtWrapper::left_image_sub_callback, this, std::placeholders::_1));
-        depth_image_sub = this->create_subscription<sensor_msgs::msg::Image>("/zed2i/zed_node/depth/depth_registered", video_qos, std::bind(&TensorrtWrapper::depth_image_sub_callback, this, std::placeholders::_1));
+        // init and load the detector
+        infer = std::make_shared<yolov5::Detector>();
+        infer->init();
+        infer->loadEngine(engine_file);
+
+        // create the pub for detections
+        detection_pub = this->create_publisher<vision_msgs::msg::Detection3DArray>("detections", rclcpp::SystemDefaultsQoS());
+
+        // create the two image subs we need to consume
+        left_image_sub = this->create_subscription<sensor_msgs::msg::Image>("/zed2i/zed_node/left/image_rect_color",
+            rclcpp::SystemDefaultsQoS(), std::bind(&TensorrtWrapper::left_image_sub_callback, this, std::placeholders::_1));
+
+        depth_image_sub = this->create_subscription<sensor_msgs::msg::Image>("/zed2i/zed_node/depth/depth_registered",
+            rclcpp::SystemDefaultsQoS(), std::bind(&TensorrtWrapper::depth_image_sub_callback, this, std::placeholders::_1));
     }
 
 private:
-    void left_image_sub_callback(const sensor_msgs::msg::Image &msg)
+    void left_image_sub_callback(const sensor_msgs::msg::Image::ConstSharedPtr& msg)
     {
-        // cv_bridge::CvImagePtr frame;
-
-        // frame = cv_bridge::toCvCopy(msg, msg.encoding);
-
-        cv::Mat frame;
+        cv::Mat frame = cv_bridge::toCvShare(msg, "bgr8")->image;
+        
+        if(frame.empty()) {
+            RCLCPP_WARN(get_logger(), "Empty image on topic %s", left_image_sub->get_topic_name());
+            return;
+        }
 
         cv::cuda::GpuMat gpu_frame;
+        std::vector<yolov5::Detection> detections;
+        
         gpu_frame.upload(frame);
+        auto res = infer->detect(frame, &detections);
 
-        infer->loadNextImage(gpu_frame);
-
-        infer->inferLoadedImg();
-
-        std::vector<tensor_detector::YoloDetect> detections;
-        infer->postProcessResults(detections);
-
+        // make the detection array and copy the header from the camera as this is all still camera relative
         vision_msgs::msg::Detection3DArray detections3d;
+        detections3d.header = msg->header;
 
-        for (tensor_detector::YoloDetect detection : detections)
+        // process each of the detections
+        for (yolov5::Detection detection : detections)
         {
-            int totalPoints = 0;
-            float totalDepth = 0;
-            for (int i = -1; i < 2; i++)
-            {
-                int u = (detection.bounds.x + (i * 3)) + detection.bounds.width / 2;
-                for (int j = -1; j < 2; j++)
-                {
-                    int v = (detection.bounds.y + (j * 3)) + detection.bounds.height / 2;
-                    int centerIdx = u + detection.bounds.width * v;
+            // int totalPoints = 0;
+            // float totalDepth = 0;
+            // for (int i = -1; i < 2; i++)
+            // {
+            //     int u = (detection.bounds.x + (i * 3)) + detection.bounds.width / 2;
+            //     for (int j = -1; j < 2; j++)
+            //     {
+            //         int v = (detection.bounds.y + (j * 3)) + detection.bounds.height / 2;
+            //         int centerIdx = u + detection.bounds.width * v;
 
-                    float depth = this->depths[centerIdx];
+            //         float depth = this->depths[centerIdx];
 
-                    if (!isnanf(depth))
-                    {
-                        totalPoints++;
-                        totalDepth += depth;
-                    }
-                }
-            }
+            //         if (!isnanf(depth))
+            //         {
+            //             totalPoints++;
+            //             totalDepth += depth;
+            //         }
+            //     }
+            // }
 
-            if (totalPoints > 0)
-            {
-                float fx = 1078.96;
-                float fy = 1079.0;
-                float cx = 967.35;
-                float cy = 544.594;
-                float depth = totalDepth / totalPoints;
+            // if (totalPoints > 0)
+            // {
+            //     float fx = 1078.96;
+            //     float fy = 1079.0;
+            //     float cx = 967.35;
+            //     float cy = 544.594;
+            //     float depth = totalDepth / totalPoints;
 
-                float xPixLoc = detection.bounds.x + detections.bounds.width / 2;
-                float yPixLoc = detection.bounds.y + detections.bounds.height / 2;
+            //     // float xPixLoc = detection.bounds.x + detections.bounds.width / 2;
+            //     // float yPixLoc = detection.bounds.y + detections.bounds.height / 2;
 
-                float xPos = (xPixLoc - cx) * depth / fx;
-                float yPos = (yPixLoc - cy) * depth / fy;
+            //     float xPixLoc = 0.0;
+            //     float yPixLoc = 0.0;
 
-                vision_msgs::msg::Detection3D detection3d;
 
-                detection3d.header.stamp = msg.header.stamp;
-                detection3d.header.frame_id = "zed2i_left_optical_frame";
+            //     float xPos = (xPixLoc - cx) * depth / fx;
+            //     float yPos = (yPixLoc - cy) * depth / fy;
 
-                geometry_msgs::msg::Pose objPose;
-                vision_msgs::msg::ObjectHypothesisWithPose objHypo;
+            //     vision_msgs::msg::Detection3D detection3d;
 
-                objHypo.hypothesis.class_id = detection.class_id;
-                objHypo.pose.pose.position.x = xPos;
-                objHypo.pose.pose.position.y = yPos;
-                objHypo.pose.pose.position.z = depth;
+            //     detection3d.header.stamp = msg.header.stamp;
+            //     detection3d.header.frame_id = "zed2i_left_optical_frame";
 
-                detection3d.results.push_back(objPose);
-                detections3d.detections.push_back(detection3d);
-            }
+            //     geometry_msgs::msg::Pose objPose;
+            //     vision_msgs::msg::ObjectHypothesisWithPose objHypo;
+
+            //     objHypo.hypothesis.class_id = detection.class_id;
+            //     objHypo.pose.pose.position.x = xPos;
+            //     objHypo.pose.pose.position.y = yPos;
+            //     objHypo.pose.pose.position.z = depth;
+
+            //     detection3d.results.push_back(objHypo);
+            //     detections3d.detections.push_back(detection3d);
+            // }
         }
         detection_pub->publish(detections3d);
     }
 
-    void depth_image_sub_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+    void depth_image_sub_callback(const sensor_msgs::msg::Image::ConstSharedPtr& msg)
     {
-        this->depths = (float *)(&msg->data[0]);
+        cv::Mat frame = cv_bridge::toCvShare(msg, "32FC1")->image;
+        
+        if(frame.empty()) {
+            RCLCPP_WARN(get_logger(), "Empty image on topic %s", depth_image_sub->get_topic_name());
+            return;
+        }
     }
 
     rclcpp::Publisher<vision_msgs::msg::Detection3DArray>::SharedPtr detection_pub;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr left_image_sub;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_image_sub;
-    std::shared_ptr<tensor_detector::YoloInfer> infer;
-    float *depths;
+    std::shared_ptr<yolov5::Detector> infer;
 };
 
 int main(int argc, char **argv)
